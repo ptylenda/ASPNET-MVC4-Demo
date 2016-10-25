940da079f9de644af55de793919df5a1507c0362
Piotr Tylenda <tylenda.piotr@gmail.com>
2016-10-25 14:33:26 -0700

    Basic controller

---
+++ 03_PhotoSharingApplication/PhotoSharingApplication/Controllers/PhotoController.cs
@@ -0,0 +1,104 @@
+﻿using PhotoSharingApplication.Filters;
+using PhotoSharingApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoSharingApplication.Controllers
+{
+    [ControllerActionAudit]
+    public class PhotoController : Controller
+    {
+        private readonly PhotoSharingContext context;
+
+        public PhotoController(PhotoSharingContext context)
+        {
+            this.context = context;
+        }
+
+        //
+        // GET: /Photo/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var model = this.context.Photos.ToList();
+            return this.View(model);
+        }
+        
+        [HttpGet]
+        public ActionResult Display(int id)
+        {
+            var photos = this.context.Photos.Find(id);
+            if (photos == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            return this.View(photos);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var photo = new Photo { CreateDate = DateTime.Now };
+            return this.View(photo);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Photo photo, HttpPostedFileBase image)
+        {
+            photo.CreateDate = DateTime.Now;
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(photo);
+            }
+
+            photo.ImageMimeType = image.ContentType;
+            photo.PhotoFile = new byte[image.ContentLength];
+            image.InputStream.Read(photo.PhotoFile, 0, image.ContentLength);
+
+            this.context.Photos.Add(photo);
+            this.context.SaveChanges();
+
+            return this.Redirect("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var photo = context.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Delete", photo);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var photo = context.Photos.Find(id);
+            context.Photos.Remove(photo);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public FileContentResult GetImage(int id)
+        {
+            Photo photo = context.Photos.Find(id);
+            if (photo != null)
+            {
+                return File(photo.PhotoFile, photo.ImageMimeType);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ 03_PhotoSharingApplication/PhotoSharingApplication/Models/Photo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PhotoSharingApplication.Models
+{
+    public class Photo
+    {
+        public int PhotoId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [DisplayName("Picture")]
+        public byte[] PhotoFile { get; set; }
+
+        public string ImageMimeType { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yy}")]
+        public DateTime CreateDate { get; set; }
+
+        public string UserName { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
