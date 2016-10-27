3d901d29c90913d8ba6ceddbb0d98fb7aa4fe991
Piotr Tylenda <tylenda.piotr@gmail.com>
2016-10-27 16:05:49 -0700

    Connecting to Azure and start of authentication provider

---
+++ 10_PhotoSharingApplication - Access controll and Azure/PhotoSharingApplication/Scripts/SlideShow.js
@@ -0,0 +1,39 @@
+﻿var SlideShowModule = function ($) {
+    var percentIncrement;
+    var percentCurrent = 100;
+
+    var init = function () {
+        window.setInterval(function () {
+            SlideShowModule.animate();
+        }, 5000);
+    };
+
+    var animate = function () {
+        var activeCard = $("#slide-show > div.active-card");
+        if (!activeCard.length) {
+            activeCard = $("#slide-show > div.slide-show-card").last();
+        }
+
+        var nextCard = activeCard.next();
+        if (!nextCard.length) {
+            nextCard = $("#slide-show > div.slide-show-card").first();
+        }
+
+        activeCard.addClass("last-active-card");
+        nextCard.css({ opacity: 0.0 });
+        nextCard.addClass("active-card");
+
+        nextCard.animate(
+            { opacity: 1.0 },
+            2000,
+            "linear",
+            function () {
+                activeCard.removeClass("active-card last-active-card");
+            });
+    };
+
+    return {
+        init: init,
+        animate: animate
+    }
+} (jQuery);
