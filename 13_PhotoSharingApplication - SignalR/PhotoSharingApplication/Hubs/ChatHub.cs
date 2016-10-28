ad491731d0dc69dbc868594496883fc514830dd3
Piotr Tylenda <tylenda.piotr@gmail.com>
2016-10-28 14:55:16 -0700

    SignalR 2.x small chat

---
+++ 13_PhotoSharingApplication - SignalR/PhotoSharingApplication/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PhotoSharingApplication.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public Task Join(int photoId)
+        {
+            return this.Groups.Add(this.Context.ConnectionId, CreateGroupName(photoId));
+        }
+
+        public Task Send(string userName, int photoId, string message)
+        {
+            string groupName = CreateGroupName(photoId);
+            return this.Clients.Group(groupName).addMessage(userName, message);
+        }
+
+        private static string CreateGroupName(int photoId)
+        {
+            return string.Format("Photo {0}", photoId);
+        }
+    }
+}
---
+++ 13_PhotoSharingApplication - SignalR/PhotoSharingApplication/OwinStartup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(PhotoSharingApplication.Startup))]
+namespace PhotoSharingApplication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
